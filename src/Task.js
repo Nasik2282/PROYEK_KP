73c692c07da4c7a6a9acba0eb020a93312746353
Nasik2282 <108223812+Nasik2282@users.noreply.github.com>
2022-12-15 22:16:47 +0700

    secondds commit

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "abcde" // TODO: replace this
+const url = "https://symphonious-moonbeam-84d65f.netlify.app" // TODO: replace this
 const studentName = "Ahmad Nasik" // TODO: replace this
 const studentId = "FE2871756" // TODO: replace this
 
